fae36c68557c4025c21aa7d3866da98b12bffae0
Callum Davidson <callum.davidson@consilium.co.nz>
2024-03-26 15:26:51 +1300

    Got it working properly with the API

--- README.md
+++ README.md
@@ -2,33 +2,57 @@
 This is a simple script that will scrape recent daily FX data from Yahoo Finance.
 
 ## Usage
-The script can be run from the command line like so:
 
-`python scrape_yahoo_fx.py --fx_type=closing --o=rates.csv --currencies USD GBP AUD`
+## Command Line Execution
 
-You can also import and use the function `scrape_yahoo_fx` to pull the data as a pandas DataFrame for use within your code.
-``` python
-from scrape_yahoo_fx import scrape_yahoo_fx
+You can run the `scrape_yahoo_fx.py` script directly from the command line. Here's how to do it:
 
-currencies = [
-    "USD",
-    "GBP",
-    "AUD"
-]
-df = scrape_yahoo_fx(fx_type = "closing", currencies=currencies)
-
-...
+```bash
+python scrape_yahoo_fx.py --fx_type=closing --from=2024-1-1 --to=2024-3-1 --interval=1d --frequency=1d --currencies USD GBP AUD --o=example_rates.csv
 ```
 
+- `--fx_type`: The type of FX data to scrape. This can be `closing`, `opening`, `high`, or `low`.
+- `--from`: The start date for the data to scrape. Suggest the format `YYYY-MM-DD`, others will work.
+- `--to`: The end date for the data to scrape. If not provided, the current date will be used.
+- `--interval`: The interval for fetching data, e.g. `1d`, `1wk`, `1mo`. If not provided, `1d` will be used.
+- `--frequency`: The frequency for fetching data, e.g. `1d`, `1wk`, `1mo`. If not provided, `1d` will be used.
+- `--currencies`: The currencies to scrape data for. This should be a space-separated list of currency codes.
+- `--o`: The name of the output file where the scraped data will be saved. This should be a `.csv` file. If not provided, the data will be printed to the console.
+
+## Using the `scrape_yahoo_fx` Function
+
+The `scrape_yahoo_fx` function allows you to scrape foreign exchange (FX) data from Yahoo Finance directly within your Python code. Here's an example of how to use it:
+
+```python
+from scrape_yahoo_fx import scrape_yahoo_fx
+from datetime import datetime
+
+# Define the currencies to scrape data for
+currencies = ["USD", "GBP", "AUD"]
+
+# Call the function
+df = scrape_yahoo_fx(
+    fx_type="closing", 
+    currencies=currencies,
+    from_date=datetime(2024,1,1),
+    to_date=datetime(2024,3,1),
+    interval='1d',
+    frequency='1d'
+)
+
+# The function returns a pandas DataFrame
+print(df.head())
+```
 The scraper will produce an output CSV/DataFrame with the following format, where each column is in the format "BASE TARGET".
 
 | Date       | USD GBP | GBP USD | USD USD | USD AUD | AUD USD | GBP AUD | AUD GBP | GBP GBP | AUD AUD |
 |------------|---------|---------|---------|---------|---------|---------|---------|---------|---------|
-| 2024-03-25 | 0.79    | 1.26    | 1.00    | 1.53    | 0.65    | 1.93    | 0.52    | 1.00    | 1.00    |
-| 2024-03-22 | 0.79    | 1.27    | 1.00    | 1.52    | 0.66    | 1.93    | 0.52    | 1.00    | 1.00    |
-| 2024-03-21 | 0.78    | 1.28    | 1.00    | 1.52    | 0.66    | 1.94    | 0.52    | 1.00    | 1.00    |
-| 2024-03-20 | 0.79    | 1.27    | 1.00    | 1.53    | 0.65    | 1.95    | 0.51    | 1.00    | 1.00    |
+| 2024-01-01 | 0.79    | 1.27    | 1.00    | 1.47    | 0.68    | 1.87    | 0.54    | 1.00    | 1.00    |
+| 2024-01-02 | 0.79    | 1.27    | 1.00    | 1.47    | 0.68    | 1.87    | 0.54    | 1.00    | 1.00    |
+| 2024-01-03 | 0.79    | 1.26    | 1.00    | 1.48    | 0.68    | 1.87    | 0.54    | 1.00    | 1.00    |
+| 2024-01-04 | 0.79    | 1.27    | 1.00    | 1.48    | 0.67    | 1.88    | 0.53    | 1.00    | 1.00    |
+| 2024-01-05 | 0.79    | 1.27    | 1.00    | 1.49    | 0.67    | 1.89    | 0.53    | 1.00    | 1.00    |
 | ...        | ...     | ...     | ...     | ...     | ...     | ...     | ...     | ...     | ...     |
 
-This will only scrape recent daily data. If people ask for it, I will spin something up that can pull historical data from different periods and at different frequencies. 
 
+Hopefully someone finds this little script useful, enjoy!
--- scrape_yahoo_fx.py
+++ scrape_yahoo_fx.py
@@ -5,107 +5,205 @@ import pandas as pd
 import aiohttp
 import asyncio
 import argparse
+from typing import Literal
+from datetime import datetime
+from dateutil.parser import parse
+import tabulate
 
 
 def _build_yahoo_fx_url(base, target):
-    return f"https://finance.yahoo.com/quote/{base}{target}%3DX/history"
+    return f"https://query1.finance.yahoo.com/v7/finance/download/{base}{target}%3DX"
 
-async def get_yahoo_fx_rate_async(session: aiohttp.ClientSession, base, target):
+
+async def get_yahoo_fx_rate_async(
+    session: aiohttp.ClientSession,
+    base: str,
+    target: str,
+    from_date: datetime,
+    to_date: datetime,
+    interval: Literal["1d", "1wk", "1mo"] = "1d",
+    frequency: Literal["1d", "1wk", "1mo"] = "1d",
+):
     url = _build_yahoo_fx_url(base, target)
-    headers = {
-        'User-Agent': 'Mozilla/5.0'
+    headers = {"User-Agent": "Mozilla/5.0"}
+    params = {
+        "period1": int(from_date.timestamp()),
+        "period2": int(to_date.timestamp()),
+        "interval": interval,
+        "filter": "history",
+        "frequency": frequency,
+        "includeAdjustedClose": "true",
     }
-    async with session.get(url, headers=headers) as response:
+    async with session.get(url, headers=headers, params=params) as response:
         response.raise_for_status()
-        html = soup.BeautifulSoup(await response.text(), 'html.parser')
-        table = html.find('table')
-        headers = [header.text for header in table.find_all('th')]
-        body = table.findNext('tbody')
-        rows = body.find_all('tr')
-        data = []
-        for row in rows:
-            data.append([cell.text for cell in row.find_all('td')])
-
+        content = await response.content.read()
+        content = content.decode()
+        rows = content.split("\n")
+        headers = rows[0].split(",")
+        data = [row.split(",") for row in rows[1:]]
         df = pd.DataFrame(data, columns=headers)
-        df['Date'] = pd.to_datetime(df['Date'])
-        df = df.set_index('Date')
-        df = df.apply(pd.to_numeric, errors='coerce')
-
+        df["Date"] = pd.to_datetime(df["Date"])
+        df = df.set_index("Date")
+        df = df.apply(pd.to_numeric, errors="coerce")
         return df
-    
-async def fetch_all(currencies):
 
+
+async def fetch_all_async(
+    currencies: list[str],
+    from_date: datetime,
+    to_date: datetime,
+    interval: Literal["1d", "1wk", "1mo"] = "1d",
+    frequency: Literal["1d", "1wk", "1mo"] = "1d",
+):
     pairs_to_fetch = []
     for i, base in enumerate(currencies):
-        for target in currencies[i+1:]:
+        for target in currencies[i + 1 :]:
             pairs_to_fetch.append((base, target))
-    
+
     fx_rates = {}
     async with aiohttp.ClientSession() as session:
         tasks = []
         for base, target in pairs_to_fetch:
-            tasks.append(get_yahoo_fx_rate_async(session, base, target))
+            tasks.append(get_yahoo_fx_rate_async(session, base, target, from_date, to_date, interval, frequency))
         results = await asyncio.gather(*tasks)
         for (base, target), df in zip(pairs_to_fetch, results):
-            fx_rates[f"{base} {target}"] = df # normal rate
-            fx_rates[f"{target} {base}"] = 1/df # reverse rate
-            fx_rates[f"{base} {base}"] = df**0 # identity rate
-    fx_rates[currencies[-1]+" "+currencies[-1]] = fx_rates[currencies[0]+" "+currencies[0]] # add the last identity rate
+            fx_rates[f"{base} {target}"] = df  # normal rate
+            fx_rates[f"{target} {base}"] = 1 / df  # reverse rate
+            fx_rates[f"{base} {base}"] = df**0  # identity rate
+
+    fx_rates[currencies[-1] + " " + currencies[-1]] = fx_rates[currencies[0] + " " + currencies[0]] # add the last identity rate - a little hacky
     return fx_rates
 
-def get_all_closing_fx_rates(currencies):
-    fx_rates = asyncio.run(fetch_all(currencies))
-    _closing_rates = {k: v['Close*'] for k, v in fx_rates.items()}
+def fetch_all(
+    currencies: list[str],
+    from_date: datetime,
+    to_date: datetime,
+    interval: Literal["1d", "1wk", "1mo"] = "1d",
+    frequency: Literal["1d", "1wk", "1mo"] = "1d",
+):
+    fx_rates = asyncio.run(fetch_all_async(currencies, from_date, to_date, interval, frequency))
+    return fx_rates
+
+
+def get_closing_fx_rates(
+        currencies: list[str],
+        from_date: datetime,
+        to_date: datetime,
+        interval: Literal["1d", "1wk", "1mo"] = "1d",
+        frequency: Literal["1d", "1wk", "1mo"] = "1d",
+):
+    fx_rates = fetch_all(currencies, from_date, to_date, interval, frequency)
+    _closing_rates = {k: v["Close"] for k, v in fx_rates.items()}
     closing_rates = pd.DataFrame(_closing_rates)
     return closing_rates
 
-def get_all_opening_fx_rates(currencies):
-    fx_rates = asyncio.run(fetch_all(currencies))
-    _opening_rates = {k: v['Open'] for k, v in fx_rates.items()}
+
+def get_opening_fx_rates(
+    currencies: list[str],
+    from_date: datetime,
+    to_date: datetime,
+    interval: Literal["1d", "1wk", "1mo"] = "1d",
+    frequency: Literal["1d", "1wk", "1mo"] = "1d",
+):
+    fx_rates = fetch_all(currencies, from_date, to_date, interval, frequency)
+    _opening_rates = {k: v["Open"] for k, v in fx_rates.items()}
     opening_rates = pd.DataFrame(_opening_rates)
     return opening_rates
 
-def get_all_high_fx_rates(currencies):
-    fx_rates = asyncio.run(fetch_all(currencies))
-    _high_rates = {k: v['High'] for k, v in fx_rates.items()}
+
+def get_high_fx_rates(
+    currencies: list[str],  
+    from_date: datetime,
+    to_date: datetime,
+    interval: Literal["1d", "1wk", "1mo"] = "1d",
+    frequency: Literal["1d", "1wk", "1mo"] = "1d",
+):
+    fx_rates = fetch_all(currencies, from_date, to_date, interval, frequency)
+    _high_rates = {k: v["High"] for k, v in fx_rates.items()}
     high_rates = pd.DataFrame(_high_rates)
     return high_rates
 
-def get_all_low_fx_rates(currencies):
-    fx_rates = asyncio.run(fetch_all(currencies))
-    _low_rates = {k: v['Low'] for k, v in fx_rates.items()}
+
+def get_low_fx_rates(
+    currencies: list[str],
+    from_date: datetime,
+    to_date: datetime,
+    interval: Literal["1d", "1wk", "1mo"] = "1d",
+    frequency: Literal["1d", "1wk", "1mo"] = "1d",
+):
+    fx_rates = fetch_all(currencies, from_date, to_date, interval, frequency)
+    _low_rates = {k: v["Low"] for k, v in fx_rates.items()}
     low_rates = pd.DataFrame(_low_rates)
     return low_rates
 
-def scrape_yahoo_fx(fx_type, currencies) -> pd.DataFrame:
-    if fx_type.lower() == 'closing':
-        df = get_all_closing_fx_rates(currencies)
-    elif fx_type.lower() == 'opening':
-        df = get_all_opening_fx_rates(currencies)
-    elif fx_type.lower() == 'high':
-        df = get_all_high_fx_rates(currencies)
-    elif fx_type.lower() == 'low':
-        df = get_all_low_fx_rates(currencies)
+
+def scrape_yahoo_fx(
+        fx_type: Literal["closing", "opening", "high", "low"],
+        currencies: list[str],
+        from_date: datetime,
+        to_date: datetime = None,
+        interval: Literal["1d", "1wk", "1mo"] = "1d",
+        frequency: Literal["1d", "1wk", "1mo"] = "1d",
+) -> pd.DataFrame:
+    """
+    Scrape foreign exchange (FX) data from Yahoo Finance.
+
+    Args:
+        fx_type (str): The type of FX data to scrape. This can be 'closing', 'opening', 'high', or 'low'.
+        currencies (list): The currencies to scrape data for. This should be a list of currency codes.
+        from_date (datetime): The start date for the data to scrape.
+        to_date (datetime, optional): The end date for the data to scrape. If not provided, the current date will be used.
+        interval (str, optional): The interval for fetching data, e.g. '1d', '1wk', '1mo'. If not provided, '1d' will be used.
+        frequency (str, optional): The frequency for fetching data, e.g. '1d', '1wk', '1mo'. If not provided, '1d' will be used.
+
+    Returns:
+        pandas.DataFrame: A DataFrame containing the scraped FX data.
+    """
+    if to_date is None:
+        to_date = datetime.now()
+    if fx_type.lower() == "closing":
+        df = get_closing_fx_rates(currencies, from_date, to_date, interval, frequency)
+    elif fx_type.lower() == "opening":
+        df = get_opening_fx_rates(currencies, from_date, to_date, interval, frequency)
+    elif fx_type.lower() == "high":
+        df = get_high_fx_rates(currencies, from_date, to_date, interval, frequency)
+    elif fx_type.lower() == "low":
+        df = get_low_fx_rates(currencies, from_date, to_date, interval, frequency)
     else:
         raise ValueError("Invalid FX rate type. Choose from closing, opening, high, low")
     return df
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Scrape Yahoo Finance for FX rates')
-    parser.add_argument('--fx_type', type=str, help='Type of FX rate to fetch', required=True)
-    parser.add_argument('--o', type=str, help='Output file', required=True)
-    parser.add_argument('--currencies', nargs='+', help='List of currencies to fetch', required=True)
+    parser = argparse.ArgumentParser(description="Scrape Yahoo Finance for FX rates")
+    parser.add_argument("--fx_type", type=str, help="Type of FX rate to fetch", required=True)
+    parser.add_argument("--from", type=str, help="Start date for fetching", required=True)
+    parser.add_argument("--to", type=str, help="End date for fetching", required=False, default=None)
+    parser.add_argument("--interval", type=str, help="Interval for fetching, e.g. 1d, 1wk, 1mo", default="1d")
+    parser.add_argument("--frequency", type=str, help="Frequency for fetching, e.g. 1d, 1wk, 1mo", default="1d")
+    parser.add_argument("--o", type=str, help="Output file for CSV, if None, displays in console", required=False, default=None)
+    parser.add_argument("--currencies", nargs="+", help="List of currencies to fetch", required=True)
     args = parser.parse_args()
     fx_type = args.fx_type
     output = args.o
     currencies = args.currencies
-    
+    from_date = parse(getattr(args, "from"))
+    to_date = None if args.to is None else parse(args.to)
+    interval = args.interval
+    frequency = args.frequency
+
     if len(currencies) < 2:
         raise ValueError("Please provide at least two currencies to fetch")
-    if output is None or output == '':
-        raise ValueError("Please provide an output file")
+    if fx_type.lower() not in ["closing", "opening", "high", "low"]:
+        raise ValueError("Invalid FX rate type. Choose from closing, opening, high, low")
+    currencies = [currency.upper() for currency in currencies]
+    if any([len(currency) != 3 for currency in currencies]):
+        raise ValueError("Currency codes must be 3 characters long")
     
-    df = scrape_yahoo_fx(fx_type, currencies)
-    df.to_csv(output)
-  
+    df = scrape_yahoo_fx(fx_type, currencies, from_date, to_date, interval, frequency)
+    if output:
+        df.to_csv(output)
+    else:
+        # print to console nicely
+        print(df.to_markdown())
+
